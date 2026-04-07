5d4b99b8fe95e6efc28900735ac434afe7d454df
Leon <zimnyleon@icloud.com>
2026-04-07 07:45:50 +0200

    Initial Commit

---
+++ SoundFlow/SoundFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundFlowApp.swift
+//  SoundFlow
+//
+//  Created by Leon Zimny on 07.04.26.
+//
+
+import SwiftUI
+
+@main
+struct SoundFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SoundFlowTests/SoundFlowTests.swift
@@ -0,0 +1,16 @@
+//
+//  SoundFlowTests.swift
+//  SoundFlowTests
+//
+//  Created by Leon Zimny on 07.04.26.
+//
+
+import Testing
+
+struct SoundFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
